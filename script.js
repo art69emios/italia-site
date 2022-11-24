ebed8f97f72c7094627749c18a53c5aeaf3a17fb
Artem <artemios69soprun@gmail.com>
2022-11-24 12:44:52 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+new WOW().init();
+
+let cookie = document.querySelector('.cookie')
+let cookieBtn = document.querySelector('.cookie-btn')
+
+cookieBtn.addEventListener('click', function(){
+   if(cookieBtn){
+      cookie.style.display = 'none'
+   }
+})
+
+
+
+cellPhn.addEventListener('mouseout', function(){
+   cellPhn.textContent = '+393517301445'
+   })
